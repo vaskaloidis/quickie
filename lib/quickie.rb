c6be76c603589cbe2e882a2eb259725c7c1f342f
vaskaloidis <vas.kaloidis@gmail.com>
2018-10-22 12:28:02 -0400

    Checkin

--- lib/quickie.rb
+++ lib/quickie.rb
@@ -9,7 +9,8 @@ class Quickie < Thor
     File.dirname(__FILE__)
   end
 
-    desc "bash NAME", "Generate a quick BASH app-skeleton"
+  # Generators
+  desc "bash NAME", "Generate a quick BASH app-skeleton"
   def bash(name)
     copy_file('./quickie/templates/bash-quick.sh.tt', "#{name}.sh") # TODO: Rename bash-quick
     `subl #{name}.sh`
@@ -39,5 +40,20 @@ class Quickie < Thor
     directory('./quickie/templates/sinatra', "#{name}")
     `subl #{name}`
   end
+
+  desc "command NAME", "Generate new quickie Command"
+  def command(name)
+    directory('./quickie/templates/sinatra', "#{name}")
+    `subl ~/Code/Ruby/quickie/`
+  end
+
+  # CLI Helper Utils
+  desc "checkin", "Checkin code quickly, commit message is optional. Master / Origin branch and remote are default unless specified with --branch <branch-name> --remote <remote-name>"
+  def checkin # message: nil, branch: 'master', remote: 'origin'
+    `git status`
+    # Add a simple yes / no here (press enter to continue or space)
+    echo `git add -A && git commit -m "Checkin" && git push origin master`
+  end
+
 
 end
