41df536dac2f5e75477951613a601533bb9cca93
vaskaloidis <vas.kaloidis@gmail.com>
2018-10-21 23:39:11 -0400

    added subl feature after generating skeleton. Fixed a bug with the relative directory of the skeletons getting generated

--- lib/quickie.rb
+++ lib/quickie.rb
@@ -11,28 +11,33 @@ class Quickie < Thor
 
     desc "bash NAME", "Generate a quick BASH app-skeleton"
   def bash(name)
-    copy_file('lib/quickie/templates/bash-quick.sh.tt', "#{name}.sh") # TODO: Rename bash-quick
+    copy_file('./quickie/templates/bash-quick.sh.tt', "#{name}.sh") # TODO: Rename bash-quick
+    `subl #{name}.sh`
   end
 
   desc "bash_lg NAME", "Generate a BASH app-skeleton"
   def bash_lg(name)
     empty_directory("#{name}")
-    copy_file('lib/quickie/templates/bash.sh.tt', "#{name}/#{name}.sh") #TODO: Rename bash to bash_lg
+    copy_file('./quickie/templates/bash.sh.tt', "#{name}/#{name}.sh") #TODO: Rename bash to bash_lg
+    `subl #{name}/`
   end
 
   desc "ruby NAME", "Generate a Ruby app-skeleton"
   def ruby(name)
-    directory('lib/quickie/templates/ruby', "#{name}")
+    directory('./quickie/templates/ruby', "#{name}")
+    `subl #{name}`
   end
 
   desc "gem NAME", "Generate a gem bundle"
   def gem(name)
     `bundle gem --test=rspec --bin #{name}`
+    `subl #{name}`
   end
 
   desc "sinatra NAME", "Generate a regular Sinatra app-skeleton"
   def sinatra(name)
-    directory('lib/quickie/templates/sinatra', "#{name}")
+    directory('./quickie/templates/sinatra', "#{name}")
+    `subl #{name}`
   end
 
 end
